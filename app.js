6473f71801edd0808b75781ff46863e151802f32
Michael Alava <malava984@gmail.com>
2024-07-15 16:52:19 -0500

    correcion cors manual 2

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ app.use((req, res, next) => {
     }
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, authorization');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH');
-    res.header('Access-Control-Allow-Credentials', 'true');
     if (req.method === 'OPTIONS') {
       return res.sendStatus(200);
     }
