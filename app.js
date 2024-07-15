ea1c1342f30f154162b99d9d938eee2bbea2cdff
Michael Alava <malava984@gmail.com>
2024-07-15 05:52:43 -0500

    correcion de cors 11

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const routes = require("./routes/main");
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors({
-    origin: 'http://localhost:5500',
+    origin: ['http://127.0.0.1:5500/', 'http://localhost:5500/'],
     allowedHeaders: ['Content-Type', 'Authorization', 'authorization'],
     methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH'] ,
     credentials: true,
